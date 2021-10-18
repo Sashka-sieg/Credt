10289eb8b387d0ce9ecbb0b950ff4d459dae51d9
Alexandr Yakushin <valensiec08@mail.ru>
2021-10-18 21:11:23 +0300

    edit

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -2,8 +2,8 @@ public class CreditPaymentService {
     public double calculate(double years, double interestRate, double creditAmount) {
         double mounth = years * 12;
         double percent = interestRate / 100 / 12;
-        double degree = Math.pow(1+percent, mounth);
-        double annuity = ((percent*degree)/(degree-1));
+        double degree = Math.pow(1 + percent, mounth);
+        double annuity = ((percent * degree) / (degree - 1));
         double monthlyPayment = Math.round(creditAmount * annuity);
         return monthlyPayment;
     }
